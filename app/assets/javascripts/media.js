78f8a89f5bf47f998e5d87733c2329c341cb2aed
Ubuntu <ubuntu@ip-172-31-41-115.ec2.internal>
2014-12-14 00:50:06 +0000

    remove missing js script. videos page still needs to have correct code for gallery

---
+++ app/assets/javascripts/media.js
@@ -0,0 +1,11 @@
+blueimp.Gallery([
+    {
+        title: 'A Vimeo video',
+        //href: 'https://vimeo.com/22495944',
+        type: 'text/html',
+        vimeo: '22495944',
+        poster: 'http://i.vimeocdn.com/portrait/2405267_300x300.jpg',
+	footer: "TEST FOOT",
+	description: "TEST DESCRIPTION"
+    },
+]);
