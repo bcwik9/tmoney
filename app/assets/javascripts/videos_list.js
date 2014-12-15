c4d949d49423b39e0c011719401a26bd791c78a5
Ubuntu <ubuntu@ip-172-31-41-115.ec2.internal>
2014-12-15 23:40:51 +0000

    fix missing thumbnail images for videos

--- app/assets/javascripts/videos_list.js
+++ app/assets/javascripts/videos_list.js
@@ -12,7 +12,7 @@ document.getElementById('vids').onclick = function (event) {
 	    href: 'https://www.youtube.com/watch?v=0hGBct3JCT0',
 	    type: 'text/html',
 	    youtube: '0hGBct3JCT0',
-	    poster: 'assets/bottleservice.jpg'
+	    poster: 'assets/thumbs/bottleservice.jpg'
 	},
 	{
             title: 'Frozen - Albion',
@@ -26,14 +26,14 @@ document.getElementById('vids').onclick = function (event) {
             href: 'https://vimeo.com/90347043',
             type: 'text/html',
             vimeo: '90347043',
-            poster: 'assets/showmeyourfreak.jpg',
+            poster: 'assets/thumbs/showmeyourfreak.jpg',
 	},
 	{
 	    title: 'Palisades Park - Counting Crows',
 	    href: 'https://www.youtube.com/watch?v=8-tFkOBU1BQ',
 	    type: 'text/html',
 	    youtube: '8-tFkOBU1BQ',
-	    poster: 'assets/palisadespark.jpg'
+	    poster: 'assets/thumbs/palisadespark.jpg'
 	},
 	
     ]);
