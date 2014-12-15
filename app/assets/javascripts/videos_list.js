5c4a1a350d54542723bfc79dc7beb869a76338c7
Ubuntu <ubuntu@ip-172-31-41-115.ec2.internal>
2014-12-15 05:42:56 +0000

    display video gallery when button is clicked

---
+++ app/assets/javascripts/videos_list.js
@@ -0,0 +1,18 @@
+document.getElementById('links').onclick = function (event) {
+    blueimp.Gallery([
+	{
+	    title: 'A YouYube video',
+	    href: 'https://www.youtube.com/watch?v=0hGBct3JCT0',
+	    type: 'text/html',
+	    youtube: '0hGBct3JCT0',
+	    poster: 'https://img.youtube.com/vi/0hGBct3JCT0/maxresdefault.jpg'
+	},
+	{
+            title: 'A Vimeo video',
+            href: 'https://vimeo.com/22495944',
+            type: 'text/html',
+            vimeo: '22495944',
+            poster: 'http://i.vimeocdn.com/portrait/2405267_300x300.jpg',
+	},
+    ]);
+}
