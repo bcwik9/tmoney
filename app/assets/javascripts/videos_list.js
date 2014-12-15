b14060c072de10e0bba49d8fd7a58c07d1e2b534
Ubuntu <ubuntu@ip-172-31-41-115.ec2.internal>
2014-12-15 09:34:30 +0000

    Add new video. Need to upload poster image for bottle service

--- app/assets/javascripts/videos_list.js
+++ app/assets/javascripts/videos_list.js
@@ -1,18 +1,26 @@
 document.getElementById('vids').onclick = function (event) {
     blueimp.Gallery([
 	{
-	    title: 'A YouYube video',
+	    title: 'Moxiie - Bottle Service',
 	    href: 'https://www.youtube.com/watch?v=0hGBct3JCT0',
 	    type: 'text/html',
 	    youtube: '0hGBct3JCT0',
-	    poster: 'https://img.youtube.com/vi/0hGBct3JCT0/maxresdefault.jpg'
+	    poster: 'tracey_bottleservice.jpg'
 	},
 	{
-            title: 'A Vimeo video',
+            title: 'Frozen - Albion',
             href: 'https://vimeo.com/22495944',
             type: 'text/html',
             vimeo: '22495944',
             poster: 'http://i.vimeocdn.com/portrait/2405267_300x300.jpg',
 	},
+	{
+            title: 'Show Me Your Freak - Blake Sicily',
+            href: 'https://vimeo.com/90347043',
+            type: 'text/html',
+            vimeo: '90347043',
+            poster: 'http://i.vimeocdn.com/portrait/90347043_300x300.jpg',
+	},
+	
     ]);
 }
